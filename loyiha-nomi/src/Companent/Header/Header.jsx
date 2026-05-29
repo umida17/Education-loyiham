b95a518cd867b0defeca8864ddbf0a36f8aaa56c
uy <a.normurzaev20@gmail.com>
2026-05-29 21:56:58 +0500

    header

--- loyiha-nomi/src/Companent/Header/Header.jsx
+++ loyiha-nomi/src/Companent/Header/Header.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import educarion from '../../assets/Edu.png'
+
+
 
 const Header = () => {
   return (
     <div>
-      
+      <div className="container">
+        <div className="header-container">
+            <img src={educarion} alt="" />
+            <div className="HOME">
+                <select>
+                    <option value="Home">Home</option>
+                    <option value="Home">Courses</option>
+                    <option value="Home">Page</option>
+                 </select>
+
+                 <select>
+                    <option value="Home">Courses</option>
+                    <option value="Home">Abaut</option>
+                    <option value="Home">Page</option>
+                 </select>
+
+                 <select>
+                    <option value="Home">Page</option>
+                    <option value="Home">Courses</option>
+                    <option value="Home">Home</option>
+                 </select>
+
+                 <select>
+                    <option value="Home">Blog</option>
+                    <option value="Home">Courses</option>
+                    <option value="Home">Page</option>
+                 </select>
+
+                 <a href="#">Contact</a>
+                 <a href="#">Singin</a>
+                 <button><a href="#">Sing Up</a></button>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
