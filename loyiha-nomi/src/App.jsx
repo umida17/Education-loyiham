021e3136892e330b08f6d18d23680c3d66a0e7b1
uy <a.normurzaev20@gmail.com>
2026-05-30 11:37:50 +0500

    section 5 css stil

--- loyiha-nomi/src/App.jsx
+++ loyiha-nomi/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Header from './Companent/Header/Header';
 import Hero from './Companent/Hero/Hero';
-import Main  from './Companent/Main/Main'; 
+import Main from './Companent/Main/Main'; 
+import Footer from './Companent/Footer';  
 
 function App() {
   const [lang, setLang] = useState('uz');
@@ -10,9 +11,10 @@ function App() {
     <div>
       <Header lang={lang} setLang={setLang} />
       
-        <Hero lang={lang} />
-        <Main lang={lang} />
-    
+      <Hero lang={lang} />
+      <Main lang={lang} />
+      
+       <Footer lang={lang} />
     </div>
   );
 }
